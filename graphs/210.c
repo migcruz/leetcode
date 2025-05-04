71bea3c0c1e2c94fcc9fc1fd923f55e1511c9dcf
Miguel Cruz <miguel.ramon.cruz@gmail.com>
2025-05-04 00:07:25 -0700

    added 210 and edited 207 with more efficient array construction

--- graphs/210.c
+++ graphs/210.c
@@ -1,27 +1,34 @@
 /*
- * LeetCode Problem 207: Course Schedule
+ * LeetCode Problem 210: Course Schedule II
  * Difficulty: Medium
  *
  * There are a total of numCourses courses labeled from 0 to numCourses - 1.
  * You are given an array prerequisites where prerequisites[i] = [ai, bi]
- * means you must take course bi before course ai.
+ * indicates that you must take course bi first if you want to take course ai.
  *
- * Return true if it is possible to finish all courses, otherwise return false.
+ * Return the ordering of courses you should take to finish all courses.
+ * If there are many valid answers, return any of them.
+ * If it is impossible to finish all courses, return an empty array.
  *
  * Example 1:
  * Input: numCourses = 2, prerequisites = [[1,0]]
- * Output: true
+ * Output: [0,1]
  *
  * Example 2:
- * Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
- * Output: false
+ * Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+ * Output: [0,2,1,3]
+ *
+ * Example 3:
+ * Input: numCourses = 1, prerequisites = []
+ * Output: [0]
  *
  * Constraints:
  *   - 1 <= numCourses <= 2000
- *   - 0 <= prerequisites.length <= 5000
+ *   - 0 <= prerequisites.length <= numCourses * (numCourses - 1)
  *   - prerequisites[i].length == 2
  *   - 0 <= ai, bi < numCourses
- *   - All the pairs prerequisites[i] are unique
+ *   - ai != bi
+ *   - All the pairs [ai, bi] are distinct
  */
 
 // Structure for adjacency list nodes
@@ -30,7 +37,7 @@ typedef struct Node {
     uint16_t val;
     uint16_t maxChildrenSize;
     uint16_t childrenSize;
-    uint16_t *children; // contains indexes in the array of Noddes
+    uint16_t *children; // contains indexes in the array of Nodes
 } Node;
 
 void addChildren(Node **nodeArr, int src, int dest) {
@@ -49,7 +56,7 @@ void addChildren(Node **nodeArr, int src, int dest) {
     }
 }
 
-int DFS(Node **nodeArr, int index, bool visited[], bool *loopDetected) {
+int DFS(Node **nodeArr, int index, bool visited[], bool *loopDetected, int **ansArr, int *ansSize) {
 
     if (nodeArr[index] == NULL) {
         return 0;
@@ -71,14 +78,18 @@ int DFS(Node **nodeArr, int index, bool visited[], bool *loopDetected) {
     // DFS on children
     for (int i = 0; i < nodeArr[index]->childrenSize; i++) {
 
-        courseCount += DFS(nodeArr, nodeArr[index]->children[i], visited, loopDetected);
+        courseCount += DFS(nodeArr, nodeArr[index]->children[i], visited, loopDetected, ansArr, ansSize);
     }
     visited[index] = false; // reset to false when backtracking so we don't get a false positive on loop detected i.e [0,1], [0,2], [1,2]. 2 will be a false positive loop
     courseCount++;
+
+    (*ansArr)[*ansSize] = index;
+    (*ansSize)++;
+
     return courseCount;
 }
 
-bool countCourses(Node **nodeArr, int size, int courses) {
+bool countCourses(Node **nodeArr, int size, int courses, int **ansArr, int *ansSize) {
 
     if (nodeArr == NULL) {
         return 0;
@@ -91,7 +102,7 @@ bool countCourses(Node **nodeArr, int size, int courses) {
         // Do DFS on this node if it has NOT been visited
         if (nodeArr[i] != NULL && !(nodeArr[i]->visited)) {
             bool visited[2000] = {false};
-            coursesToTake += DFS(nodeArr, i, visited, &loopDetected);
+            coursesToTake += DFS(nodeArr, i, visited, &loopDetected, ansArr, ansSize);
             if (loopDetected) {
                 // printf("loopdetected at %d\n", i);
                 return false;
@@ -122,22 +133,24 @@ void printAllChildren(Node **nodeArr, int size) {
     }
 }
 
-bool canFinish(int numCourses, int** prerequisites, int prerequisitesSize, int* prerequisitesColSize) {
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findOrder(int numCourses, int** prerequisites, int prerequisitesSize, int* prerequisitesColSize, int* returnSize) {
     
-    // if you are forced to revisit a course then return false (i.e. a cycle in the directional graph)
-    // the highest level course will have its preReqs as children
-    // use dfs but only increase the counter indicating that you took that course after completing dfs for that node so that we count the prereqs first
-    // store the nodes in an array and we will do dfs on each of them
-    // use visited array to make sure we dont repeat courses and to detect loops, reset this array afetr every fresh DFS run
-    // use another array or a struct member to keep track if node is visited so we dont double count it
+    // we'll use course schedule I to help with this problem (problem 207)
+    // we will add every preReq course to a course as their children
+    // then we will dfs and add the children first in the array as we recurse on the way up
 
     if (prerequisites == NULL) {
-        return false;
+        return NULL;
     }
 
     Node **nodeArr = (Node**) calloc (2000, sizeof(Node*)); // max size of prerequisitesSize * 2 in case there are 2 unique courses per entry
+    int ansSize = 0;
+    int *ansArr = (int*) calloc (2000, sizeof(int)); // just set to 2000 so we dont have to resize
 
-    // fill nodeArr
+    // fill nodeArr 
     for (int i = 0; i < numCourses; i++) {
         Node *newNode = (Node*) malloc (sizeof(Node));
         newNode->visited = false;
@@ -155,7 +168,13 @@ bool canFinish(int numCourses, int** prerequisites, int prerequisitesSize, int* 
 
     // printAllChildren(nodeArr, 2000); // Sanity check that the graph is constructed properly
 
-    bool ans = countCourses(nodeArr, 2000, numCourses);
+    bool isPossible = countCourses(nodeArr, 2000, numCourses, &ansArr, &ansSize);
 
-    return ans;
+    if (!isPossible) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    *returnSize = ansSize;
+    return ansArr;
 }
